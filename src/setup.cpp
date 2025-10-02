5b5dd68fdb62523a7d250f3b76f04d384a8efae7
Mario Petkov <mario.18@abv.bg>
2025-10-02 20:17:40 +0300

    Working CFD with floor.

--- src/setup.cpp
+++ src/setup.cpp
@@ -5,60 +5,61 @@ void main_setup() {
     std::ofstream csv("results.csv");
     csv << "t,Fx,Fy,Fz,Tx,Ty,Tz\n";
 
-    const uint3 lbm_N = resolution(float3(1.0f, 2.0f, 0.5f), 2000u);
+    const uint3 lbm_N = resolution(float3(1.0f, 2.0f, 0.5f), 1280u);
     const float lbm_Re = 1000000.0f;
     const float lbm_u = 0.075f;
-    const ulong lbm_T = 1000ull;
+    const ulong lbm_T = 10000ull;
 
     LBM lbm(lbm_N, units.nu_from_Re(lbm_Re, (float)lbm_N.x, lbm_u));
+    units.set_m_kg_s(1.0f, 1.0f, 1.0f);   
 
     const float size = 1.0f * lbm.size().x;
     const float3 center = float3(lbm.center().x, 0.55f * size, lbm.center().z);
     const float3x3 rotation = float3x3(float3(1, 0, 0), radians(0.0f));
 
-    lbm.voxelize_stl(get_exe_path() + "stl/car_step_model.stl", center, rotation, size);
+    lbm.voxelize_stl(get_exe_path() + "../stl/car_step_model.stl", center, rotation, size, TYPE_S | TYPE_X);
+
+    const float3 lbm_com = lbm.object_center_of_mass(TYPE_S | TYPE_X);
 
     const uint Nx = lbm.get_Nx(), Ny = lbm.get_Ny(), Nz = lbm.get_Nz();
+
+    const uint floor_thickness = 1u;
+    const uint floor_z_start = (uint)(0.25f * (float)Nz);
+    const uint floor_z_end = floor_z_start + floor_thickness;
 
     parallel_for(lbm.get_N(), [&](ulong n) {
         uint x = 0u, y = 0u, z = 0u;
         lbm.coordinates(n, x, y, z);
 
-        if (lbm.flags[n] != TYPE_S) lbm.u.y[n] = lbm_u;
-
-        // Boundaries
-        if (x == 0u || x == Nx - 1u || y == 0u || y == Ny - 1u || z == 0u || z == Nz - 1u) {
-            lbm.flags[n] = TYPE_E;
+        if (z >= floor_z_start && z <= floor_z_end && lbm.flags[n] != TYPE_S) {
+            lbm.flags[n] = TYPE_S;
         }
+
+        if (lbm.flags[n] != TYPE_S) lbm.u.y[n] = lbm_u;
+        if (x == 0u || x == Nx - 1u || y == 0u || y == Ny - 1u || z == 0u || z == Nz - 1u)
+            lbm.flags[n] = TYPE_E;
         });
+
 
     lbm.run(0u, lbm_T);
     while (lbm.get_t() < lbm_T) {
-        const float3 lbm_force = lbm.object_force(TYPE_S);
-        const float3 lbm_torque = lbm.object_torque(lbm.center(), TYPE_S);
 
-        const float si_force_x = units.si_F(lbm_force.x);   // Drag
-        const float si_force_y = units.si_F(lbm_force.y);   // Lift
-        const float si_force_z = units.si_F(lbm_force.z);   // Side force
-        const float si_torque_x = units.si_T(lbm_torque.x);
-        const float si_torque_y = units.si_T(lbm_torque.y);
-        const float si_torque_z = units.si_T(lbm_torque.z);
+        const float3 lbm_force = lbm.object_force(TYPE_S | TYPE_X);
+        const float3 lbm_torque = lbm.object_torque(lbm_com, TYPE_S | TYPE_X);
 
-        csv << lbm.get_t() << ","
-            << si_force_x << ","
-            << si_force_y << ","
-            << si_force_z << ","
-            << si_torque_x << ","
-            << si_torque_y << ","
-            << si_torque_z << "\n";
+        const float Fx = units.si_F(lbm_force.x);
+        const float Fy = units.si_F(lbm_force.y);
+        const float Fz = units.si_F(lbm_force.z);
+        const float Tx = units.si_T(lbm_torque.x);
+        const float Ty = units.si_T(lbm_torque.y);
+        const float Tz = units.si_T(lbm_torque.z);
+
+        csv << lbm.get_t() << "," << Fx << "," << Fy << "," << Fz << ","
+            << Tx << "," << Ty << "," << Tz << "\n";
 
         std::cout << "t=" << lbm.get_t()
-            << " | Fx=" << si_force_x
-            << " Fy=" << si_force_y
-            << " Fz=" << si_force_z
-            << " | Tx=" << si_torque_x
-            << " Ty=" << si_torque_y
-            << " Tz=" << si_torque_z
+            << " | Fx=" << Fx << " Fy=" << Fy << " Fz=" << Fz
+            << " | Tx=" << Tx << " Ty=" << Ty << " Tz=" << Tz
             << std::endl;
 
         lbm.run(1u, lbm_T);
