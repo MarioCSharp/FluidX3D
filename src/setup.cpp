53fafdf2cdcb9f237ef2c32b29f11c054e1f351b
Mario Petkov <mario.18@abv.bg>
2026-03-06 07:06:26 +0200

    new setup

--- src/setup.cpp
+++ src/setup.cpp
@@ -1,4 +1,8 @@
 #include "setup.hpp"
+
+#include <fstream>
+#include <iomanip>
+
 void main_setup() {
     const float si_x = 2.0f;        
     const float si_u = 30.0f;       
@@ -12,15 +16,15 @@ void main_setup() {
     units.set_m_kg_s(lbm_x, lbm_u, lbm_rho, si_x, si_u, si_rho);
     const float lbm_nu = units.nu(si_nu);
 
-    const uint Nx = 512u;  
-    const uint Ny = 256u;  
+    const uint Nx = 356u;  
+    const uint Ny = 236u;  
     const uint Nz = 128u;  
     LBM lbm(Nx, Ny, Nz, lbm_nu);
 
     const float3 center = float3(128.0f, lbm.center().y, lbm.center().z);
     const float3x3 rotation = float3x3(float3(0, 0, 1), radians(-90.0f)); 
 
-    lbm.voxelize_stl(get_exe_path() + "../stl/mirror_step_model.stl", center, rotation, 64.0f, TYPE_S | TYPE_X);
+    lbm.voxelize_stl(get_exe_path() + "stl/mirror_step_model.stl", center, rotation, 64.0f, TYPE_S | TYPE_X);
 
     const uint Nx_lbm = lbm.get_Nx(), Ny_lbm = lbm.get_Ny(), Nz_lbm = lbm.get_Nz();
     parallel_for(lbm.get_N(), [&](ulong n) {
@@ -46,8 +50,17 @@ void main_setup() {
 
     lbm.run(0u); 
 
+    std::ofstream csv("results.csv");
+    csv << "drag,lift,downforce,drag_coef,lift_coef\n";
+    csv << std::fixed << std::setprecision(6);
+
     for (uint i = 0; i < 50; i++) {
         lbm.run(100u); 
+
+        if (i == 30)
+        {
+            lbm.u.write_device_to_vtk(get_exe_path(), true);
+        }
 
         const float3 lbm_force = lbm.object_force(TYPE_S | TYPE_X);
 
@@ -66,8 +79,20 @@ void main_setup() {
         const float Cd = si_force.x / (dynamic_pressure * frontal_area);
         const float Cl = si_force.z / (dynamic_pressure * frontal_area);
 
+        const float drag = si_force.x;
+        const float lift = si_force.z;
+        const float downforce = -si_force.z;
+
+		csv << drag << ","
+            << lift << ","
+            << downforce << ","
+            << Cd << ","
+            << Cl << "\n";
+
         print_info("Drag coefficient Cd: " + to_string(Cd));
         print_info("Lift coefficient Cl: " + to_string(Cl));
         print_info("Downforce: " + to_string(-si_force.z) + " N");
     }
+
+    csv.close();
 }
